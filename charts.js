74ecdd51499a3fdaaad68db38c4d58d3c42c3218
NRalston <Neil.Ralston@MSPMAC.ORG>
2022-06-11 19:32:40 -0500

    challenge through deliverable 2

--- charts.js
+++ charts.js
@@ -97,11 +97,12 @@ function buildCharts(sample) {
     Plotly.newPlot("bar", barData, barLayout)
 
     // 1. Create the trace for the bubble chart.
+              
     var bubbleData = [
       {
-        x: yticks,
-        y: xticks,
-        text: labels,
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
         mode: 'markers',
         marker: {
           size: sample_values,
@@ -112,10 +113,13 @@ function buildCharts(sample) {
 
     // 2. Create the layout for the bubble chart.
     var bubbleLayout = {
-      title: "Bacteria Cultures per Sample"
+      title: "Bacteria Cultures per Sample",
+      xaxis: {title: 'OTU ID #'},
+      hovermode: 'closest',
     };
+      
+      // 3. Use Plotly to plot the data with the layout.
 
-    // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
   })
 }
